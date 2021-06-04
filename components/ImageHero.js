658cf6623857dd8397a5090a001ae32189867c87
dc <danielmcollins1996@gmail.com>
2021-06-04 13:47:56 -0600

    removed border on hero

--- components/ImageHero.js
+++ components/ImageHero.js
@@ -2,7 +2,7 @@
 const ImageHero = ({imageFilename, bgPosition= 'bg-center', children}) => {
 
     return (
-        <div className={`h-screen grid bg-cover ${bgPosition} bg-no-repeat border-white`} style={{
+        <div className={`h-screen grid bg-cover ${bgPosition} bg-no-repeat`} style={{
             backgroundImage: `url('/images/${imageFilename}')`}}>
                 {children}
         </div>
