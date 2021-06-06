9daf4d836ce08201325c69a981f8309e77fb5deb
dc <danielmcollins1996@gmail.com>
2021-06-05 22:44:15 -0600

    animation and styling

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { motion } from "framer-motion";
 import Header from '../components/header'
 import ImageHero from '../components/ImageHero'
 
 export default function Home() {
+
   return (
     <div className="flex flex-col bg-gray-500">
       <Head>
@@ -15,21 +17,27 @@ export default function Home() {
         imageFilename='1-starlinkcharcoal.jpg'
         fixed={true}
       >
-        <div className="absolute grid max-w-4xl w-11/12 text-white text-center left-1/2	top-45 transform -translate-y-2/4 -translate-x-2/4 animate-bounce">
-          <h2 className="mb-8 text-2xl font-medium lg:mt-12 lg:mb-32 md:text-5xl">Order Starlink</h2>
-          <p className="text-xs font-light mb-8 px-12 md:text-sm">Starlink is available to a limited number of users per coverage area at this time. Orders will be fulfilled on a first-come, first-served basis.</p>
-          <form action="" className="max-w-2xl m-auto w-11/12">
-            <div className="relative">
-              <label htmlFor="service-input" className="absolute pl-4 pt-4 text-mainLabel text-xs">Service Address</label>
-              <input type="text" id="service-input" className="w-full pt-8 pb-2 pl-4 my-2 bg-mainInputBg bg-opacity-90 outline-none border border-transparent focus:border-white transition duration-300"/>
-            </div>
-            <div className="text-xs font-light text-888 mt-1.5 mb-10">Can't find your address? Try a <Link href=""><a className="underline">Plus Code</a></Link> with City.</div>
-            <button className="w-full bg-black bg-opacity-50 text-gray-600 border-2  border-gray-600 h-14 md:w-64" type="submit">Order Now</button>
-          </form>
-        </div>
-          <div className="absolute w-full text-center bottom-7">
-            <p><span className="text-xs font-light text-888">By clicking ORDER NOW, you agree to our <Link href="https://www.starlink.com/legal/privacy-policy"><a className="text-white">Privacy Policy</a></Link>.</span></p>
-          </div>
+            <motion.div
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.5 }}
+            >
+              <div className="absolute grid max-w-4xl w-11/12 mt-6 text-white text-center left-1/2	top-45 transform -translate-y-2/4 -translate-x-2/4">
+                <h2 className="mb-8 text-4xl font-medium lg:mt-12 lg:mb-32 md:text-5xl">Order Starlink</h2>
+                <p className="text-xs font-light mb-2 px-4 sm:mb-8 sm:px-12 md:text-base">Starlink is available to a limited number of users per coverage area at this time. Orders will be fulfilled on a first-come, first-served basis.</p>
+                <form action="" className="max-w-2xl m-auto w-11/12">
+                  <div className="relative">
+                    <label htmlFor="service-input" className="absolute pl-4 pt-4 text-mainLabel text-xs">Service Address</label>
+                    <input type="text" id="service-input" className="w-full pt-8 pb-2 pl-4 my-2 bg-mainInputBg bg-opacity-90 outline-none border border-transparent focus:border-white transition duration-300"/>
+                  </div>
+                  <div className="text-xs font-light text-888 mt-1.5 mb-10">Can't find your address? Try a <Link href=""><a className="underline">Plus Code</a></Link> with City.</div>
+                  <button className="w-full bg-black bg-opacity-50 cursor-not-allowed text-gray-600 border-2  border-gray-600 h-14 md:w-64" type="submit">Order Now</button>
+                </form>
+              </div>
+            </motion.div>
+                <div className="absolute w-full text-center bottom-7">
+                  <p><span className="text-xs font-light mx-2 text-888">By clicking ORDER NOW, you agree to our <Link href="https://www.starlink.com/legal/privacy-policy"><a className="text-white">Privacy Policy</a></Link>.</span></p>
+                </div>
       </ImageHero>
       <ImageHero 
         bgPosition='bg-left' 
@@ -46,7 +54,11 @@ export default function Home() {
           </div>
         </div>
       </ImageHero>
-      <ImageHero />
+      <ImageHero
+        imageFilename='6-Starlink_Nightsky-cropped.jpg'
+      >
+
+      </ImageHero>
       <div className="inline-block h-12 bg-red-400">
         hey
       </div>
